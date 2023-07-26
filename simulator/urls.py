aa3260a4fd424c4b3e79da4c3eacf070d4880f62
robin.mueller-bady <robin.mueller-bady@cyber-curriculum.com>
2023-07-26 10:57:28 +0200

    Created initalizer function for DroneTypes and Drones and included it in GUI as button

--- simulator/urls.py
+++ simulator/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'simulator'
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('start/', views.start, name='start'),
+    path('init/', views.init, name='init'),
 ]
--- simulator/views.py
+++ simulator/views.py
@@ -5,6 +5,7 @@ from simulator.serializers import DroneSerializer, DroneTypeSerializer, DroneDyn
 from simulator.models import Drone, DroneType, DroneDynamics
 from rest_framework.decorators import action
 from rest_framework.response import Response
+import random
 
 # REST API views
 class DroneViewSet(viewsets.ModelViewSet):
@@ -37,4 +38,48 @@ def index(request):
     drones = Drone.objects.all()
     context = {'drones': drones}
     return render(request, 'simulator/index.html', context)
+
+def start(request):
+    return HttpResponse("Started")
+
+def create_serial_number(dronetype):
+    name = dronetype.manufacturer[0:2] + dronetype.typename[0:2]
+    year = str(random.randint(2020, 2031))
+    serial = ('%06x' % random.randrange(16**6)).upper()
+    return '-'.join([name, year, serial])
+
+def init(request):
+    if Drone.objects.count() > 0:
+        return HttpResponse("Database already initialized. Delete database before calling init")
+    dronetypes = [
+            DroneType(manufacturer="GoPro", typename="Karma", weight=1000, max_speed=56, battery_capacity=5100, control_range=1500, max_carriage=400),
+            DroneType(manufacturer="Hubsan", typename="X4 H107D", weight=50, max_speed=32, battery_capacity=380, control_range=200, max_carriage=50),
+            DroneType(manufacturer="Walkera", typename="Voyager 4", weight=2450, max_speed=80, battery_capacity=7500, control_range=5000, max_carriage=800),
+            DroneType(manufacturer="PowerVision", typename="PowerEgg X", weight=2100, max_speed=64, battery_capacity=5000, control_range=3500, max_carriage=500),
+            DroneType(manufacturer="Blade", typename="Chroma Camera Drone", weight=1630, max_speed=65, battery_capacity=5400, control_range=2500, max_carriage=600),
+            DroneType(manufacturer="DJI", typename="Phantom 4 Pro", weight=1380, max_speed=72, battery_capacity=5870, control_range=7000, max_carriage=500),
+            DroneType(manufacturer="Yuneec", typename="Typhoon H Pro", weight=1995, max_speed=70, battery_capacity=5400, control_range=2000, max_carriage=100),
+            DroneType(manufacturer="Autel Robotics", typename="Evo II", weight=1127, max_speed=72, battery_capacity=7100, control_range=9000, max_carriage=800),
+            DroneType(manufacturer="Parrot", typename="Anafi", weight=320, max_speed=55, battery_capacity=2700, control_range=4000, max_carriage=200),
+            DroneType(manufacturer="Skydio", typename="Skydio 2", weight=775, max_speed=58, battery_capacity=4280, control_range=3500, max_carriage=400),
+            ]
+    drones = [
+            Drone(dronetype=dronetypes[0], serialnumber=create_serial_number(dronetypes[0]), carriage_weight=200, carriage_type="SEN"),
+            Drone(dronetype=dronetypes[1], serialnumber=create_serial_number(dronetypes[1]), carriage_weight=0, carriage_type="NOT"),
+            Drone(dronetype=dronetypes[2], serialnumber=create_serial_number(dronetypes[2]), carriage_weight=400, carriage_type="SEN"),
+            Drone(dronetype=dronetypes[3], serialnumber=create_serial_number(dronetypes[3]), carriage_weight=100, carriage_type="SEN"),
+            Drone(dronetype=dronetypes[4], serialnumber=create_serial_number(dronetypes[4]), carriage_weight=300, carriage_type="ACT"),
+            Drone(dronetype=dronetypes[5], serialnumber=create_serial_number(dronetypes[5]), carriage_weight=150, carriage_type="SEN"),
+            Drone(dronetype=dronetypes[6], serialnumber=create_serial_number(dronetypes[6]), carriage_weight=0, carriage_type="NOT"),
+            Drone(dronetype=dronetypes[7], serialnumber=create_serial_number(dronetypes[7]), carriage_weight=0, carriage_type="NOT"),
+            Drone(dronetype=dronetypes[8], serialnumber=create_serial_number(dronetypes[8]), carriage_weight=0, carriage_type="NOT"),
+            Drone(dronetype=dronetypes[9], serialnumber=create_serial_number(dronetypes[9]), carriage_weight=300, carriage_type="ACT"),
+            ]
+
+    for i in dronetypes:
+        i.save()
+    for i in drones:
+        i.save()
+    return HttpResponse("Initialized with {} dronetypes and {} drones!".format(len(dronetypes), len(drones)))
+
 
