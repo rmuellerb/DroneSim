dd4401ed02360e973c089ffe54e1f275b9752719
robin.mueller-bady <robin.mueller-bady@cyber-curriculum.com>
2023-10-19 13:03:29 +0200

    Added also support for redoc API documentation

--- dronesim/urls.py
+++ dronesim/urls.py
@@ -52,6 +52,7 @@ urlpatterns = [
         path('accounts/', include('django.contrib.auth.urls')),
         re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
         path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
