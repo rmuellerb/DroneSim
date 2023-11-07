c5f3bf0d6e05f23daae3d46092fe0057619e0ef4
Robin Mueller-Bady <robin.mueller-bady@fb2.fra-uas.de>
2023-11-07 17:09:23 +0100

    Added support for static files in production environment

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -12,8 +12,9 @@ then
 fi
 
 #python manage.py flush --no-input
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input --clear
 
 exec "$@"
 
